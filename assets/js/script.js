4cee23791bcc63c9e6f9a1c3fc7166ac50ae73af
Ayva Valenti <94766574+ayvavalenti@users.noreply.github.com>
2022-03-18 15:22:51 -0400

    just realized i made an html quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,83 @@ var correct = function(val, rightAns, form, span){
     }
     
     function correctAns (){
-        
+        if (val == rightAns){
+            score = +10;
+        }
+        else{
+            score = -5;
+        }
+
     }
+    // questions - all quesitons found on https://www.javatpoint.com/javascript-mcq
+
+    var ques = [
+        //q1
+        {
+            q: 'Which one of the following also known as Conditional Expression:',
+            o: ['Alternative to if-else', 'Switch statment', 'If-then-else statement', 'Immediate if'],
+            a: "Immediate if"
+        },
+        //q2
+        {
+            q:'In JavaScript, what is a block of statement?',
+            o:['Conditional block','block that combines a number of statements into a single compound statement','both conditional block and a single statement','block that contains a single statement'],
+            a:"block that combines a number of statements into a single compound statement"
+        },
+
+        //q3
+        {
+            q:'When interpreter encounters an empty statements, what it will do:',
+            o:['Shows a warning','Throws an error', 'Ignores the statement','Prompts to complete the statement'],
+            a:"Ignores the statement"
+        },
+
+        //q4
+        {
+            q:'The "function" and " var" are known as:',
+            o:['Data types','Prototypes','Declaration statements','keywords'],
+            a:"Declaration statements"
+        },
+
+        //q5
+        {
+            q:'Which of the following variables takes precedence over the others if the names are the same?',
+            o:['The local element','Global variable','Both the local element and global variable','none of these'],
+            a:"The local element"
+        },
+
+        //q6
+        {
+            q:'In the JavaScript, which one of the following is not considered as an error:',
+            o:['Division by zero','Missing of Bracket','Missing of semicolons','Syntax error'],
+            a:"Division by zero"
+        },
+
+        //q7
+        {
+            q:'Which one of the following is not a keyword:',
+            o:['with','if','debugger','use strict'],
+            a:"use strict"
+        },
+
+        //q8
+        {
+            q:'A collection of elements of the same data type which may either in order or not, is called',
+            o:['String','Serialized Object','Array','Object'],
+            a:"Array"
+        },
+
+        //q9
+        {
+            q:'What is the basic purpose of the "toLocateString()" method?',
+            o:['It returns a localized string representation of the object','It return a parsed string','It return a local time in the string format','It returns a localised object representation'],
+            a:"It returns a localized string representation of the object"
+        }
+    ]
+
     
 
-    if (val == rightAns){
-        score = ()
-    }
+
     
    
 
